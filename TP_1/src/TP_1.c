9251d917dd6cedfb6aecf38422eddd81b97517e2
BarbaraDiaz2022 <barbydiaz2000@gmail.com>
2022-09-19 23:59:48 -0300

    prueba 1

---
+++ TP_1/src/TP_1.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : TP_1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+
+	printf("algo1");
+	return EXIT_SUCCESS;
+}
