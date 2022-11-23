9c51d628fdc455578f2a4cf388f64123841ae27e
BarbaraDiaz2022 <barbydiaz2000@gmail.com>
2022-11-23 09:25:20 -0300

    posible resolucion del punto 8 y 9 con filter y map

--- TP_3/main.c
+++ TP_3/main.c
@@ -162,6 +162,140 @@ int main()
             	if(flagCarga == 1)
             	{
 
+            		/*
+            		 * brief recibe una lista y llama a la funcion criterio para determinar si un elemento sera agregado a la nueva lista
+            		 * \param pList LinkedList* Puntero a la lista
+            		 * \param pFunc (*pFunc) Puntero a la funcion criterio
+            		 * \return retorna la linkedlist con los items correspondientes
+            		 * LinkedList* ll_filter(LinkedList* this, int(*pFun)(void* element))
+            		{
+            			int len;
+            			void* auxElemento;
+            			LinkedList* newLinkedList = NULL; //la nueva lista que va a ser retornada
+
+            			newLinkedList = ll_newLinkedList(); //la creo de forma dinamica
+
+            			if(this != NULL && pFun != NULL)
+            			{
+            				len = ll_len(this);
+
+            				for(int i = 0; i < len; i++)
+            				{
+            					auxElemento = ll_get(this, i);
+
+            					if(pFun(auxElemento) == 0)
+            					{
+            						ll_add(newLinkedList, auxElemento);
+            					}
+            				}
+            			}
+
+            			return newLinkedList;
+            		}
+
+            		 * brief recibe una lista y llama a la funcion criterio para determinar si un elemento sera agregado a la nueva lista
+            		 * \param pList LinkedList* Puntero a la lista
+            		 * \param pFunc (*pFunc) Puntero a la funcion criterio
+            		 * \return retorna la linkedlist con los campos promedios correspondientes agregados a la nueva lista
+            		LinkedList* ll_map(LinkedList* this, void*(*pFun)(void* element))
+            		{
+            			int len;
+            			LinkedList* newLinkedList = ll_newLinkedList();
+            			Jugador* auxElement;
+
+            			if(this != NULL && pFun != NULL)
+            			{
+            				len = ll_len(this);
+
+            				for(int i = 0; i < len; i++)
+            				{
+            					auxElement = ll_get(this, i);
+
+            					pFun(auxElement); //le paso el elemento y calcula el campo promedio
+
+            					ll_add(newLinkedList, auxElement);
+            				}
+            			}
+
+            			return newLinkedList;
+            		}*/
+
+            		/*case dondeusellsort:
+            			if(ll_isEmpty(lista)==0)
+            			{
+            				listaPrimeraClase = ll_filter(lista, primeraClase);
+            				if(controller_saveAsPassengersFirstClass("PPrimeraClase.csv", listaPrimeraClase)==0)
+            				{
+            					printf("\nSe han cargado correctamente los pasajeros de primera clase\n");
+            				}
+            			}
+            			else
+            			{
+            				printf("\nNo hay pasajeros cargados\n");
+            			}
+            			break;
+            		case dondeusellmap:
+            			if(ll_isEmpty(lista)==0)
+            			{
+            				listaConMillas = ll_map(lista, calcular);
+            				controller_ListPassengerWithMillas(listaConMillas);
+            			}
+            			else
+            			{
+            				printf("\nNo hay pasajeros cargados para filtrar\n");
+            			}
+            			break;
+
+            		int primeraClase(void* element)
+            		{
+            			int retorno = -1;
+            			int auxTipoDePasajero;
+
+            			if(element != NULL)
+            			{
+            				Passenger_getTipoPasajero((Passenger*)element, &auxTipoDePasajero);
+            				if(auxTipoDePasajero == 1) //si el getter me traia 1 era porque el pasajero era de primera clase entonces retorno que si pertenece
+            				{
+            					retorno = 0;
+            				}
+
+            			}
+
+            			return retorno;
+            		}
+
+
+
+            		void calcular(void* element)
+            		{
+            		//	Passenger* auxPassenger;
+            			float auxPrecio;
+            			int auxTipoDePasajero;
+            			float millas;
+
+            			if(element != NULL)
+            			{
+            				Passenger_getPrecio((Passenger*)element, &auxPrecio);
+            				Passenger_getTipoPasajero((Passenger*)element, &auxTipoDePasajero);
+
+            				millas = auxPrecio / 100;
+
+            				if(auxTipoDePasajero == 1)
+            				{
+            					millas = millas * 2;
+            				}
+            				else if(auxTipoDePasajero == 2)
+            				{
+            					millas = millas * 3;
+            				}
+
+            				Passenger_setMillas((Passenger*)element, millas);
+            			}
+            		}
+
+            		 *
+            		 * */
+
             	}
             	else
             	{
